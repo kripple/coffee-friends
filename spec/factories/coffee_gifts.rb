79af77ba4b100a7ed95e459d394e74b9146371b0
Kelly Ripple <kelly.m.ripple@gmail.com>
2024-12-06 10:03:45 -0500

    remove spec during versioning

--- spec/factories/coffee_gifts.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do 
-
-	factory :coffee_gift do 
-		message {Faker::Lorem.paragraph}
-	end
-		
-end	
--- spec/factories/picture_uploaders.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :picture_uploader do
-    
-  end
-
-end
